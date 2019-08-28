c2ac70caf4c6c68041aa1a6ff808139c286861eb
unknown <sarbojit.project@gmail.com>
2019-08-28 13:40:14 -0700

    Cleaned it a little - console.logs.

--- routes/dashboard/saveGetGroupMembers.js
+++ routes/dashboard/saveGetGroupMembers.js
@@ -7,6 +7,9 @@ const router = express.Router();
 const dbDebugger = require("debug")("app:db");
 const logger = require("../../utils/loggerSetup");
 
+// Utlity sort
+const msort = require("../../utils/sortOn");
+
 // DB Schemas
 const Group = require("../../models/group");
 const User = require("../../models/user");
@@ -28,7 +31,7 @@ router.post("/", async (req, res) => {
   // });
   let ret = await Group.find( filter );
 
-  console.log("ret before:", ret[0]);
+  dbDebugger("ret before:", ret[0]);
 
   if (req.body.requestType === "AddSelectMembers") {
 
@@ -39,17 +42,17 @@ router.post("/", async (req, res) => {
 
     if (result.length === 0) {
       // if emial does not exist in the members array -- add member
-      console.log('Will update new member :', req.body.member.email);
+      dbDebugger('Will update new member :', req.body.member.email);
       let bid = 0;
       if (req.body.member.baandaId === 0) {
         let bidget = await User.find({ email: req.body.member.email }).select(
           "-_id baandaId"
         );
-        // console.log('>>>>>>>>>>>>>>>>>>>>>> bidget:', bidget);
+        // dbDebugger('>>>>>>>>>>>>>>>>>>>>>> bidget:', bidget);
         // Need to test when we form few baanda members and use them for new member.
         if (bidget.length !== 0) {
           bid = bidget[0].baandaId;
-          // console.log('found baanaId for email:', bidget[0].baandaId);
+          // dbDebugger('found baanaId for email:', bidget[0].baandaId);
         }
       } else {
         bid = req.body.member.baandaId;
@@ -65,7 +68,7 @@ router.post("/", async (req, res) => {
         joinDate: Date.now(),
         role: req.body.member.role
       };
-      // console.log('newMember:', newMember);
+      // dbDebugger('newMember:', newMember);
       let filter = {
         communityId: req.body.communityId,
         groupId: req.body.groupId
@@ -77,18 +80,20 @@ router.post("/", async (req, res) => {
       output = pushRet.members;
     } else {
       // if member exists - ignore and send back the member list as is
-      console.log('Will NOT update new member :', req.body.member.email);
+      dbDebugger('Will NOT update new member :', req.body.member.email);
       output = ret[0].members;
     }
   } else {
-    console.log('Just asked for member list :');
+    dbDebugger('Just asked for member list :');
     output = ret[0].members;
-    // console.log("%%% output:", output);
   }
 
-  let yy = output.sort(sortOn("memberName", req.body.ascDsc));
-  // console.log("### output:", output);
-  res.send(yy);
+  let sorted = msort(output, "memberName", req.body.ascDsc);
+  res.status(200).send(sorted);
+
+  // let yy = output.sort(sortOn("memberName", req.body.ascDsc));
+  // dbDebugger("### output:", output);
+  // res.send(yy);
 });
 
 module.exports = router;
